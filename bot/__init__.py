2f6109d73d09ab9b6e17323b7d28762902e54ef6
bisaek <huggerbertram@gmail.com>
2025-03-27 11:06:31 +0100

    bot

---
+++ bot/__init__.py
@@ -0,0 +1,14 @@
+import chess
+
+from bot.searcher import Searcher
+from bot.transposition_table import TranspositionTable
+
+
+class Bot:
+    def __init__(self):
+        self.transposition_table = TranspositionTable()
+
+    def best_move(self, board: chess.Board):
+        self.transposition_table.board = board
+        searcher = Searcher(board, self.transposition_table, 20)
+        return searcher.start_search()
